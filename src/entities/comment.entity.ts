627bf1824d2aabab9f53faf11311d75703d5ca82
Your Name <you@example.com>
2024-08-22 07:13:38 +0545

    [200~feat(comments): implement pagination for comment replies

--- src/entities/comment.entity.ts
+++ src/entities/comment.entity.ts
@@ -6,21 +6,16 @@ import {User} from '../entities/user/user.entity'
 class Comment extends Base {
     @Column()
     comment: string;
-
     @ManyToOne(() => Notes, (note) => note.comments, { onDelete: 'CASCADE' })
     @JoinColumn({ name: 'post_id' })
     note: Notes;
-
     @ManyToOne(() => Comment, (comment) => comment.replies)
     @JoinColumn({ name: 'parent_id' })
     parent: Comment | null;
-
     @OneToMany(() => Comment, (comment) => comment.parent)
     replies: Comment[];
-
     @ManyToOne(() => User, (user) => user.comments, { nullable: true })
     @JoinColumn({ name: 'user_id' })
     user: User | null;
 }
-
 export default Comment;
