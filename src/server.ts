57a91dc4c92c37e87919939324b43d582f512a4d
Your Name <you@example.com>
2024-09-18 06:45:09 +0545

    dfs

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,26 @@
-import { createServer } from 'http'
-import app from './config/app.config'
-import { AppDataSource } from './config/database.config'
-import { DotenvConfig } from './config/env.config'
-import Print from './utils/print'
-import { ChatSocket } from './socket/chatSocket'
+import {createServer} from 'http';
+import app from './config/app.config';
+import {AppDataSource} from './config/database.config';
+import {DotenvConfig} from './config/env.config';
+import Print from './utils/print';
+import {ChatSocket} from './socket/chatSocket';
 
-const chatSocket = new ChatSocket()
+const chatSocket = new ChatSocket();
 
 function listen() {
-  const PORT = DotenvConfig.PORT
-  const httpServer = createServer(app)
-  chatSocket.setupSocket(httpServer)
-  httpServer.listen(PORT)
-  Print.info(`🚀 Server is listening on port ${DotenvConfig.PORT}`)
+  const PORT = DotenvConfig.PORT;
+  const httpServer = createServer(app);
+  chatSocket.setupSocket(httpServer);
+  httpServer.listen(PORT);
+  Print.info(`🚀 Server is listening on port ${DotenvConfig.PORT}`);
 }
 AppDataSource.initialize()
   .then(async () => {
-    Print.info(`🚀 Database successfully connected`)
-    listen()
+    Print.info(`🚀 Database successfully connected`);
+
+    listen();
   })
   .catch((err) => {
-    Print.error(`❌ Database connection failure - ${err?.message}`)
-  })
+    Print.error(`❌ Database connection failure - ${err?.message}`);
+  }
+)
