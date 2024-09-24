63d5e9800bfb495ade8da93cb5eb142550d21320
Your Name <you@example.com>
2024-09-24 11:05:14 +0545

    fixed chat issue

--- src/services/room.service.ts
+++ src/services/room.service.ts
@@ -1,7 +1,7 @@
-import { In } from 'typeorm';
-import { AppDataSource } from '../config/database.config';
-import { User } from '../entities/user/user.entity';
-import { Room } from '../entities/room.entity';
+import {In} from 'typeorm';
+import {AppDataSource} from '../config/database.config';
+import {User} from '../entities/user/user.entity';
+import {Room} from '../entities/room.entity';
 
 export default class RoomService {
   private readonly roomRepository = AppDataSource.getRepository(Room);
@@ -10,32 +10,29 @@ export default class RoomService {
     const room = await this.roomRepository
       .createQueryBuilder('room')
       .leftJoin('room.participants', 'participants')
-      .where('participants.id IN (:...participants)', { participants })
+      .where('participants.id IN (:...participants)', {participants})
       .groupBy('room.id')
-      .having('COUNT(participants.id) = :participantCount', { participantCount: participants.length })
+      .having('COUNT(participants.id) = :participantCount', {participantCount: participants.length})
       .getOne();
-  
+
     if (room) {
       console.log(`Room found: ${room.id}`); // Debug log
       return room;
     } else {
-      const users = await User.findBy({ id: In(participants) });
-      const newRoom = this.roomRepository.create({ participants: users });
+      const users = await User.findBy({id: In(participants)});
+      const newRoom = this.roomRepository.create({participants: users});
       const savedRoom = await this.roomRepository.save(newRoom);
       console.log(`Room created: ${savedRoom.id}`); // Debug log
       return savedRoom;
     }
   }
-  
-
   async findRoom(participants: string[]) {
     return await this.roomRepository
       .createQueryBuilder('room')
       .leftJoin('room.participants', 'participants')
-      .where('participants.id IN (:...participants)', { participants })
+      .where('participants.id IN (:...participants)', {participants})
       .groupBy('room.id')
-      .having('COUNT(participants.id) = :participantCount', { participantCount: participants.length })
+      .having('COUNT(participants.id) = :participantCount', {participantCount: participants.length})
       .getOne();
   }
-  
 }
